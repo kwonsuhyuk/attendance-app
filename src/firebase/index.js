42ac381eb39ee914fd0836dfca581a29d8aca5c1
YOOJEHYEON <jehyunyoo@ajou.ac.kr>
2023-12-23 17:29:15 +0900

    first commit

---
+++ src/firebase/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBWsGya53buGyFjmgeV-dNgd2XqeBXy4d0',
+  authDomain: 'britec-attd-app.firebaseapp.com',
+  projectId: 'britec-attd-app',
+  storageBucket: 'britec-attd-app.appspot.com',
+  messagingSenderId: '723788106509',
+  appId: '1:723788106509:web:4cb593996a6105ca1bfe44',
+  measurementId: 'G-P4SQ6BEGGM',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
