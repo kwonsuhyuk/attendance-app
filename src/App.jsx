9fd7664f2094029d0198ab979829505d8604f598
YOOJEHYEON <jehyunyoo@ajou.ac.kr>
2024-01-13 23:00:28 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import CssBaseline from "@mui/material/CssBaseline";
-import { useEffect } from "react";
-import "./App.css";
-import SignupPage from "./Page/SignupPage";
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
-import LoginPage from "./Page/LoginPage";
-import Notfound from "./Page/Notfound";
-import MainPage from "./Page/MainPage";
-import ManagerFirstPage from "./Page/signupProcessPage/ManagerFirstPage";
-import EmployeeFirstPage from "./Page/signupProcessPage/EmployeeFirstPage";
-import IndexPage from "./Page/IndexPage";
-import AccessCameraPage from "./Page/AccessCameraPage";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { ClipLoader } from "react-spinners";
-import "./firebase";
-import { useDispatch, useSelector } from "react-redux";
-import { clearUser, setUser } from "./store/userSlice";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import CssBaseline from '@mui/material/CssBaseline';
+import { useEffect } from 'react';
+import './App.css';
+import SignupPage from './Page/SignupPage';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import LoginPage from './Page/LoginPage';
+import Notfound from './Page/Notfound';
+import MainPage from './Page/MainPage';
+import ManagerFirstPage from './Page/signupProcessPage/ManagerFirstPage';
+import EmployeeFirstPage from './Page/signupProcessPage/EmployeeFirstPage';
+import IndexPage from './Page/IndexPage';
+import AccessCameraPage from './Page/AccessCameraPage';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { ClipLoader } from 'react-spinners';
+import './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearUser, setUser } from './store/userSlice';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const dispatch = useDispatch();
@@ -39,12 +39,12 @@ function App() {
 
   if (isLoading) {
     return (
-      <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <div className='flex flex-col justify-center items-center h-screen w-screen'>
         <ClipLoader
-          color="black"
+          color='black'
           size={100}
-          aria-label="Loading Spinner"
-          data-testid="loader"
+          aria-label='Loading Spinner'
+          data-testid='loader'
         />
         <h3>로딩 중입니다.</h3>
       </div> // 로딩 스피너
@@ -54,20 +54,20 @@ function App() {
   return (
     <>
       <ToastContainer
-        position="bottom-right"
-        theme="light"
+        position='bottom-right'
+        theme='light'
         pauseOnHover
         autoClose={1500}
       />
       <Routes>
-        <Route path={"/"} element={<IndexPage />} />
+        <Route path={'/'} element={<IndexPage />} />
         <Route path={`/:id`} element={<MainPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/managerfirst" element={<ManagerFirstPage />} />
-        <Route path="/employeefirst" element={<EmployeeFirstPage />} />
-        <Route path="/:id/camera" element={<AccessCameraPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/managerfirst' element={<ManagerFirstPage />} />
+        <Route path='/employeefirst' element={<EmployeeFirstPage />} />
+        <Route path='/:id/camera' element={<AccessCameraPage />} />
         <Route
-          path="/signin"
+          path='/signin'
           element={
             currentUser ? (
               <Navigate to={`/${currentUser?.photoURL}`} />
--- src/Components/QR/QrScan.jsx
+++ src/Components/QR/QrScan.jsx
@@ -3,67 +3,69 @@ import React, { useState, useRef, useEffect } from 'react';
 import { Html5QrcodeScanner } from 'html5-qrcode';
 import { QrReader } from 'react-qr-reader';
 
-// function QrScan() {
-//   const [scanResult, setScanResult] = useState(null);
-
-//   useEffect(() => {
-//     const scanner = new Html5QrcodeScanner('reader', {
-//       qrbox: {
-//         width: 250,
-//         height: 250,
-//       },
-//       fps: 5,
-//     });
-
-//     scanner.render(success, error);
-//     function success(result) {
-//       scanner.clear();
-//       setScanResult(result);
-//     }
-//     function error(err) {
-//       console.warn(err);
-//     }
-//   }, []);
-
-//   return (
-//     <div className='App'>
-//       <h1>Qr code scanning in react</h1>
-//       {scanResult ? (
-//         <div>
-//           Success: <a href={'http://' + scanResult}>{scanResult}</a>
-//         </div>
-//       ) : (
-//         <div id='reader'></div>
-//       )}
-//     </div>
-//   );
-// }
-
 function QrScan() {
-  const [attendance, setAttendance] = useState('출석하지 않음');
+  const [scanResult, setScanResult] = useState(null);
 
-  const handleScan = (data) => {
-    if (data) {
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
+
+    scanner.render(success, error);
+    function success(result) {
       const now = new Date();
+      scanner.clear();
       console.log(now);
+      setScanResult(result);
     }
+    function error(err) {
+      console.warn(err);
+    }
+  }, []);
 
-    const handleError = (err) => {
-      console.error(err);
-    };
-
-    return (
-      <div>
-        <QrReader
-          delay={300}
-          onError={handleError}
-          onScan={handleScan}
-          style={{ width: 250, height: 250 }}
-        />
-        <p>{attendance}</p>
-      </div>
-    );
-  };
+  return (
+    <div className='App'>
+      <h1>Qr code scanning in react</h1>
+      {scanResult ? (
+        <div>
+          Success: <a href={'http://' + scanResult}>{scanResult}</a>
+        </div>
+      ) : (
+        <div id='reader'></div>
+      )}
+    </div>
+  );
 }
+
+// function QrScan() {
+//   const [attendance, setAttendance] = useState('출석하지 않음');
+
+//   const handleScan = (data) => {
+//     if (data) {
+//       const now = new Date();
+//       console.log(now);
+//     }
+
+//     const handleError = (err) => {
+//       console.error(err);
+//     };
+
+//     return (
+//       <div>
+//         <QrReader
+//           delay={300}
+//           onError={handleError}
+//           onScan={handleScan}
+//           style={{ width: 250, height: 250 }}
+//         />
+//         <p>{attendance}</p>
+//       </div>
+//     );
+//   };
+// }
 
 export default QrScan;
