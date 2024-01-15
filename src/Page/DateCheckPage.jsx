985182a69dfdf7246eeed21998f9880b5bf837e6
YOOJEHYEON <jehyunyoo@ajou.ac.kr>
2024-01-15 21:07:50 +0900

    adsf

--- src/Page/DateCheckPage.jsx
+++ src/Page/DateCheckPage.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import MyDatePicker from '../Components/Calendar/DatePicker';
 import MyCalendar from '../Components/Calendar/MyCalendar';
-=======
-import React from "react";
-import MyDatePicker from "../Components/Calendar/MyDatePicker";
-import MyCalendar from "../Components/Calendar/MyCalendar";
->>>>>>> 0b443bccdadf1b6bee959d8a73ae2ca5e30c589d
 
 const DateCheckPage = () => {
   return (
