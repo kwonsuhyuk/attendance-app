5cde903ee7c26ace8ed9d8b368dcfc16bcefe54c
YOOJEHYEON <jehyunyoo@ajou.ac.kr>
2024-01-26 00:33:12 +0900

    qr작동

--- src/Components/ShowSalary/ShowSalary.jsx
+++ src/Components/ShowSalary/ShowSalary.jsx
@@ -1,14 +1,12 @@
-
 import { getDatabase, onValue, ref } from 'firebase/database';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-
 function ShowSalary() {
   const [daySalary, setDaySalary] = useState(0);
   const [nightSalary, setNightSalary] = useState(0);
-  const {currentUser} = useSelector(state=>state.user)
-  
+  const { currentUser } = useSelector((state) => state.user);
+
   const companyCode = currentUser.photoURL; // 회사 코드
   const userId = currentUser.uid; // 유저 아이디
 
@@ -16,7 +14,18 @@ function ShowSalary() {
   const nightTimeWage = hourlyWage * 1.5; // 야간 시급
 
   useEffect(() => {
-    const dbref = ref(getDatabase(), `companyCode/${companyCode}/users/${userId}`);
+    const dbref = ref(
+      getDatabase(),
+      `companyCode/${companyCode}/users/${userId}`
+    );
+    const nightStartRef = ref(
+      getDatabase(),
+      `companyCode/${companyCode}/companyInfo/nightStart`
+    );
+    const nightEndRef = ref(
+      getDatabase(),
+      `companyCode/${companyCode}/companyInfo/nightEnd`
+    );
 
     onValue(dbref, (snapshot) => {
       if (snapshot.exists()) {
@@ -27,8 +36,8 @@ function ShowSalary() {
 
         if (
           start.getDate() === end.getDate() &&
-          start.getHours() < 22 &&
-          end.getHours() < 22
+          start.getHours() > nightEndRef &&
+          end.getHours() < nightStartRef
         ) {
           setDaySalary(workHours * hourlyWage);
           setNightSalary(0);
@@ -41,18 +50,14 @@ function ShowSalary() {
   }, [companyCode, userId, hourlyWage, nightTimeWage]);
 
   return (
-
     <div>
       {daySalary > 0 && <h1>당신의 오늘 주간 급여는 {daySalary}원 입니다.</h1>}
       {nightSalary > 0 && (
         <h1>당신의 오늘 야간 급여는 {nightSalary}원 입니다.</h1>
       )}
-
       ShowSalary
-
     </div>
   );
 }
-
 
 export default ShowSalary;
