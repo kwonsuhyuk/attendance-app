70733b13fd2410de1b34c38d91f29c6e235ddac8
YOOJEHYEON <jehyunyoo@ajou.ac.kr>
2024-02-27 12:18:23 +0900

    라벨링 추가

--- src/Page/ShowCalendarPage.jsx
+++ src/Page/ShowCalendarPage.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import MyCalendar from "../Components/Calendar/MyCalendar";
-import ShowSalary from "../Components/ShowSalary/ShowSalary";
-import { useMatch, useNavigate } from "react-router-dom";
-import { Divider } from "@mui/material";
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import MyCalendar from '../Components/Calendar/MyCalendar';
+import ShowSalary from '../Components/ShowSalary/ShowSalary';
+import { useMatch, useNavigate } from 'react-router-dom';
+import { Divider } from '@mui/material';
 
 const ShowCalendarPage = () => {
   const navigate = useNavigate();
@@ -22,8 +22,19 @@ const ShowCalendarPage = () => {
 
   return (
     <div className="flex flex-col items-center justify-between my-3 h-full">
-      <div className="flex items-center justify-center mb-10">
+      <div className="flex items-center justify-center mb-3">
         <MyCalendar />
+      </div>
+      <div className="flex flex-row justify-start mr-auto mb-5">
+        <div className="bg-red-500 font-medium text-base w-6 h-6 flex items-center justify-center">
+          0+
+        </div>
+        <div className="bg-yellow-500 font-medium text-base w-6 h-6 flex items-center justify-center">
+          4+
+        </div>
+        <div className="bg-green-500 font-medium text-base w-6 h-6 flex items-center justify-center">
+          8+
+        </div>
       </div>
       <div className="w-full">
         <ShowSalary matchHome={matchHome} matchCalendar={matchCalendar} />
