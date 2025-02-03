21ba4122a07cb6ec61e3bf4372afde58f0135f49
mintaek <omt610@gmail.com>
2025-02-03 18:15:16 +0900

    ✨Feat: shadcn, react-hook-form 라이브러리 설치 및 기능 테스트

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
+  assetsInclude: ["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.svg"],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    },
+  },
 });
