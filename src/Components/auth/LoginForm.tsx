fac4a9489c53ac8df10547383fe11f12a8453f89
mintaek <omt610@gmail.com>
2025-02-11 22:03:02 +0900

    🐛Fix: 로그인/회원가입 폼 비밀번호 텍스트가 안보이는 현상 수정

--- src/Components/auth/LoginForm.tsx
+++ src/Components/auth/LoginForm.tsx
@@ -1,5 +1,4 @@
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 import { UseFormRegister, FieldErrors } from "react-hook-form";
 import { TLoginForm } from "@/model";
