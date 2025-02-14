16310654a4729a00bfb196385aa7b114bc17ddb7
mintaek <omt610@gmail.com>
2025-02-14 14:26:15 +0900

    🐛Fix: Firebase auth관련 수정

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
-import 'firebase/database';
-import * as firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import "firebase/database";
+import * as firebase from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,19 +12,19 @@ import * as firebase from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBWsGya53buGyFjmgeV-dNgd2XqeBXy4d0',
-  authDomain: 'britec-attd-app.firebaseapp.com',
-  databaseURL:
-    'https://britec-attd-app-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'britec-attd-app',
-  storageBucket: 'britec-attd-app.appspot.com',
-  messagingSenderId: '723788106509',
-  appId: '1:723788106509:web:4cb593996a6105ca1bfe44',
-  measurementId: 'G-P4SQ6BEGGM',
+  apiKey: "AIzaSyBWsGya53buGyFjmgeV-dNgd2XqeBXy4d0",
+  authDomain: "britec-attd-app.firebaseapp.com",
+  databaseURL: "https://britec-attd-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "britec-attd-app",
+  storageBucket: "britec-attd-app.appspot.com",
+  messagingSenderId: "723788106509",
+  appId: "1:723788106509:web:4cb593996a6105ca1bfe44",
+  measurementId: "G-P4SQ6BEGGM",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = firebase.getDatabase;
 
-export { db };
+export { db, auth };
--- src/model/index.ts
+++ src/model/index.ts
@@ -2,18 +2,16 @@ import { z } from "zod";
 
 // 로그인 스키마 정의 (유효성 검사)
 export const loginFormSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
+  email: z.string().email({ message: "유효한 이메일을 입력하세요." }),
+  password: z.string().min(6, { message: "비밀번호는 최소 6자 이상이어야 합니다." }),
 });
 
 export type TLoginForm = z.infer<typeof loginFormSchema>;
 
 // 기본 회원가입 정보 스키마
 const signupUserBaseSchema = z.object({
-  name: z
-    .string()
-    .min(2, { message: "이름은 2자 이상이어야 합니다." })
-    .regex(/^[가-힣a-zA-Z\s]+$/, { message: "올바른 이름 형식이 아닙니다" }),
+  name: z.string().min(2, { message: "이름은 2자 이상이어야 합니다." }),
+  // .regex(/^[가-힣a-zA-Z\s]+$/, { message: "올바른 이름 형식이 아닙니다" }),
   companyCode: z.string().min(5, { message: "올바른 회사 코드를 입력해주세요" }),
   phoneNumber: z.string().regex(/^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/, {
     message: "올바른 전화번호 형식이 아닙니다 (예: 010-1234-5678)",
@@ -24,7 +22,6 @@ export const signupFormSchema = loginFormSchema
   .merge(signupUserBaseSchema)
   .extend({
     confirmPW: z.string().min(1, { message: "비밀번호 확인을 입력해주세요" }),
-    companyCode: z.string().min(5, { message: "올바른 회사 코드를 입력해주세요" }),
   })
   .refine(data => data.password === data.confirmPW, {
     message: "비밀번호가 일치하지 않습니다",
@@ -74,14 +71,6 @@ export type TCompanyInfo = {
   nightStart: number;
   payCheckDay: number;
   qrValue: string;
-};
-
-export type TSignUpResponse = {
-  success: boolean;
-  data?: {
-    userId: string;
-  };
-  error?: string;
 };
 
 // 날짜별 근무 시간 타입
