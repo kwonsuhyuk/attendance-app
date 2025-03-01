75fa895ccd5553114c2de81f8f03e3cfdd76c8a4
suhyuk <101502480+kwonsuhyuk@users.noreply.github.com>
2025-03-01 21:43:59 +0900

    🎨Style: 다크모드일때 색상 지정 tailwindconfig 수정 (#85)

--- src/Components/common/menubar/ManagerMenuBar.tsx
+++ src/Components/common/menubar/ManagerMenuBar.tsx
@@ -1,42 +1,40 @@
 import { Button } from "@/components/ui/button";
-import { Switch } from "@/components/ui/switch";
 import {
   Sidebar,
   SidebarContent,
   SidebarHeader,
   SidebarFooter,
-  SidebarProvider,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { Moon, Sun, LogOut } from "lucide-react";
-import { cn } from "@/util/cn.util";
+import { LogOut } from "lucide-react";
+
 import { useMenuBar } from "@/hooks/menu/useMenuBar";
 import ManagerMenuBarList from "./manager/ManagerMenuBarList";
 import AppTitle from "../AppTitle";
 
 export const ManagerMenuBar = () => {
-  const { companyLogo, companyName, darkMode, toggleTheme, logout } = useMenuBar();
+  const { logout } = useMenuBar();
   const { isMobile } = useSidebar();
 
   return (
     <Sidebar
       side={isMobile ? "right" : "left"}
-      className="h-screen w-64 border-r border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-[#1E1E1E]"
+      className="h-screen w-64 border-r border-gray-200 bg-white-bg dark:border-gray-100 dark:bg-dark-bg"
     >
-      <SidebarHeader className="flex h-16 items-center justify-center gap-2 bg-dark-bg p-4 font-bold">
+      <SidebarHeader className="flex h-16 items-center justify-center gap-2 border-solid border-dark-border bg-dark-card-bg p-4 font-bold dark:bg-dark-bg">
         <AppTitle className="text-white" />
       </SidebarHeader>
 
-      <SidebarContent className="overflow-y-auto">
+      <SidebarContent className="overflow-y-auto dark:bg-dark-bg">
         <ManagerMenuBarList />
       </SidebarContent>
 
-      <SidebarFooter className="mt-auto border-t border-gray-200 p-4 dark:border-gray-800">
+      <SidebarFooter className="mt-auto border-t border-gray-200 p-4 dark:border-gray-800 dark:bg-dark-bg">
         <Button
           variant="default"
           size="sm"
           onClick={logout}
-          className="flex w-full items-center justify-center gap-2 border-gray-300 text-gray-700 dark:border-gray-700 dark:text-gray-300"
+          className="flex w-full items-center justify-center gap-2 border-gray-300 text-gray-700 dark:border dark:border-solid dark:border-dark-border-sub dark:bg-dark-bg dark:text-dark-text hover:dark:bg-dark-card-bg"
         >
           <LogOut className="h-4 w-4" />
           <span>로그아웃</span>
