002d26dc3b07d26bec5b40279ab0bde6aae56c64
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2025-03-11 22:27:31 +0900

    🧪Test: input stories

--- src/Components/ui/input.tsx
+++ src/Components/ui/input.tsx
@@ -11,10 +11,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            // "flex h-12 w-full rounded-lg bg-background p-2 px-0 py-2 text-base",
             "flex h-12 w-full rounded-lg bg-background bg-white p-2 py-2 text-base",
             "border-[0px] border-slate-200",
             "shadow-[0_0_0_1px_rgba(0,0,0,0.2)]",
+            "dark:bg-dark-border-sub dark:text-white-bg dark:placeholder-dark-border",
             "focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2",
             "disabled:cursor-not-allowed disabled:opacity-50",
             className,
