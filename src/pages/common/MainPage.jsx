c31a07b97e9e69c3cb7ece21d46be0ea9bfd20d1
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2025-03-12 22:19:17 +0900

    🚑!HOTFIX: MainPage 실시간 구독 useEffect 등록

--- src/pages/common/MainPage.jsx
+++ src/pages/common/MainPage.jsx
@@ -3,53 +3,47 @@ import { useEffect, useState } from "react";
 import { useTour } from "@reactour/tour";
 import { useUserStore } from "@/store/user.store";
 import { useCompanyStore } from "@/store/company.store";
-import Loading from "@/components/common/Loading";
 import ManagerRoutes from "@/components/company/\bManagerRoutes";
 import EmployeeRoutes from "@/components/employee/EmployeeRoutes";
-import { getDatabase, off, onValue, ref } from "firebase/database";
 import { useParams } from "react-router-dom";
+import { getCompanyInfo, subscribeToData } from "@/api";
+import { getCompanyInfoPath } from "@/constants/api.path";
+import Loading from "@/components/common/Loading";
 
 const MainPage = () => {
   const userType = useUserStore(state => state.currentUser?.userType);
   const { companyCode } = useParams();
   const setCompany = useCompanyStore(state => state.setCompany);
-  const [isLoading, setIsLoading] = useState(false);
   const { setIsOpen } = useTour();
+  const [isLoading, setIsLoading] = useState(false);
 
-  // useEffect(() => {
-  //   async function getCompany() {
-  //     if (companyCode) {
-  //       setIsLoading(true);
-  //       try {
-  //         const data = await getCompanyInfo(companyCode);
-  //         setCompany(data);
-  //       } catch (error) {
-  //         console.error("Error fetching company info:", error);
-  //       } finally {
-  //         setIsLoading(false);
-  //       }
-  //     }
-  //   }
-  //   getCompany();
-  // }, [setCompany, companyCode]);
+  useEffect(() => {
+    async function getCompany() {
+      if (companyCode) {
+        setIsLoading(true);
+        try {
+          const data = await getCompanyInfo(companyCode);
+          setCompany(data);
+        } catch (error) {
+          console.error("Error fetching company info:", error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    }
+    getCompany();
+  }, [setCompany, companyCode]);
 
   useEffect(() => {
     if (!companyCode) return;
 
-    setIsLoading(true);
-    const db = getDatabase();
-    const companyRef = ref(db, `companyCode/${companyCode}/companyInfo`);
-
-    const unsubscribe = onValue(companyRef, snapshot => {
-      if (snapshot.exists()) {
-        const updatedCompanyData = snapshot.val();
-        console.log("변경된 회사 정보:", updatedCompanyData);
+    const unsubscribe = subscribeToData(getCompanyInfoPath(companyCode), updatedCompanyData => {
+      if (updatedCompanyData) {
         setCompany(updatedCompanyData);
       }
-      setIsLoading(false);
     });
 
-    return () => off(companyRef, "value", unsubscribe);
+    return () => unsubscribe();
   }, [companyCode, setCompany]);
 
   useEffect(() => {
@@ -61,9 +55,7 @@ const MainPage = () => {
     }
   }, [setIsOpen]);
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  if (isLoading) return <Loading />;
 
   return <>{userType === "manager" ? <ManagerRoutes /> : <EmployeeRoutes />}</>;
 };
