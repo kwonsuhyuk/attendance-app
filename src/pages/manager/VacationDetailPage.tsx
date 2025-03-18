3dbe7654419cdb55f066f77aabb23cb687c4c2e7
mintaek <omt610@gmail.com>
2025-03-18 17:26:26 +0900

    🚚Rename: 메뉴바 휴가 부분 리네임

--- src/pages/manager/VacationDetailPage.tsx
+++ src/pages/manager/VacationDetailPage.tsx
@@ -10,12 +10,12 @@ const VacationDetailPage = () => {
 
   return (
     <div className="p-4">
-      <h1 className="mb-4 text-xl font-bold">휴가 신청</h1>
+      <h1 className="mb-4 text-xl font-bold">휴가 등록</h1>
       <button
-        className="rounded border-none bg-point-color px-4 py-2 text-black"
+        className="cursor-pointer rounded border-none bg-point-color px-4 py-2 text-black"
         onClick={toggleModal}
       >
-        휴가 신청하기
+        휴가 등록 +
       </button>
 
       {isModalOpen && <VacationModal onClose={toggleModal} />}
