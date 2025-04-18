4d92fe0cbab9b84d8af5c7581459085abb6a917c
mintaek <omt610@gmail.com>
2025-04-18 21:19:50 +0900

    🗑️Remove: 콘솔, 주석 제거

--- src/hooks/manager/usePeriodAttendance.ts
+++ src/hooks/manager/usePeriodAttendance.ts
@@ -68,14 +68,12 @@ const usePeriodAttendance = (employeeList: EmployeeInfo[] = []) => {
         }
 
         const raw = await fetchCommutesByPeriod(companyCode, selectedEmployee.uid, year, month);
-        console.log("불러온 데이터", raw);
         const converted = mapCommuteDataToCalendar(raw, currentDate, workPlacesList, holidayList);
-        console.log("달력 데이터", converted);
+
         setCalendar(converted);
       }
     };
 
-    console.log("선택된 직원: ", selectedEmployee);
     fetchData();
   }, [tab, currentDate, workplaceFilter, selectedEmployee, companyCode]);
 
@@ -97,54 +95,3 @@ const usePeriodAttendance = (employeeList: EmployeeInfo[] = []) => {
 };
 
 export default usePeriodAttendance;
-
-// 직원 탭용 데이터 변환 함수
-// function mapCommuteDataToCalendar(
-//   data: Record<string, TCommuteData> | null,
-//   currentDate: Date,
-//   workPlacesList: TWorkPlace[] = [],
-//   holidayList: string[] = [],
-// ): (TCalendarDayInfo | null)[] {
-//   const daysInMonth = dayjs(currentDate).daysInMonth();
-//   const startOfMonth = dayjs(currentDate).startOf("month").day();
-//   const totalCells = Math.ceil((startOfMonth + daysInMonth) / 7) * 7;
-
-//   const year = dayjs(currentDate).format("YYYY");
-//   const month = dayjs(currentDate).format("MM");
-
-//   return Array.from({ length: totalCells }, (_, i) => {
-//     const day = i - startOfMonth + 1;
-//     if (day < 1 || day > daysInMonth) return null;
-
-//     const key = String(day).padStart(2, "0");
-//     const commute = data?.[key];
-
-//     const startPlace =
-//       workPlacesList.find(p => p.id === commute?.startWorkplaceId)?.name || "근무지";
-//     const endPlace = workPlacesList.find(p => p.id === commute?.endWorkplaceId)?.name || "근무지";
-//     const isCompanyHoliday = holidayList.includes(`${year}-${month}-${key}`);
-
-//     return {
-//       day,
-//       summary: {
-//         출근: commute?.startTime ? 1 : 0,
-//         외근: commute?.startWorkplaceId === "외근" ? 1 : 0,
-//         휴가: 0,
-//         총원: commute ? 1 : 0,
-//       },
-//       checkIn: commute?.startTime
-//         ? {
-//             time: dayjs(commute.startTime).format("HH:mm"),
-//             workplace: startPlace,
-//           }
-//         : undefined,
-//       checkOut: commute?.endTime
-//         ? {
-//             time: dayjs(commute.endTime).format("HH:mm"),
-//             workplace: endPlace,
-//           }
-//         : undefined,
-//       isCompanyHoliday,
-//     };
-//   });
-// }
