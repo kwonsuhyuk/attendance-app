b512e00cc0e9ee7bcbda8da3f67c41d87abec0fe
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2025-05-13 14:36:23 +0900

    ✨Feat: 수동 출퇴근 기능

--- src/Components/ui/DatePicker.tsx
+++ src/Components/ui/DatePicker.tsx
@@ -7,36 +7,35 @@ import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { cn } from "@/util/cn.util";
+import { twMerge } from "tailwind-merge";
 
 interface DatePickerDemoProps {
   pickDate: Date;
   setPickDate: (date: Date) => void;
+  className?: string;
 }
 
-export function DatePickerDemo({ pickDate, setPickDate }: DatePickerDemoProps) {
+export function DatePickerDemo({ pickDate, setPickDate, className }: DatePickerDemoProps) {
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
-          className={cn(
-            "w-full justify-start text-left font-normal sm:w-[240px]",
+          className={twMerge(
+            "w-full justify-start text-left font-normal",
             !pickDate && "text-muted-foreground",
+            className,
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
           {pickDate ? format(pickDate, "PPP", { locale: ko }) : <span>날짜 선택</span>}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0" align="start">
+      <PopoverContent className="w-full min-w-0 p-0" align="start">
         <Calendar
           mode="single"
           selected={pickDate}
-          onSelect={date => {
-            if (date) {
-              setPickDate(date);
-            }
-          }}
+          onSelect={date => date && setPickDate(date)}
           initialFocus
           locale={ko}
           modifiers={{ today: new Date() }}
