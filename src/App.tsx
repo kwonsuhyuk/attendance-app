bf9a1386be65f875ca93bb497b12d01e50ea7af4
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2025-05-30 22:31:27 +0900

    🐛Fix: 새로고침시 메인으로 가는버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./firebase";
+import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-import Loading from "./components/common/Loading";
+
 import MainRoutes from "./routes/MainRoutes";
+import Loading from "./components/common/Loading";
+import { useUserStore } from "@/store/user.store";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getUser } from "@/api";
+import { TCMUserData, TEmpUserData } from "@/model/types/user.type";
 
 const App = () => {
-  return <RouterProvider router={MainRoutes} fallbackElement={<Loading />} />;
+  const [appReady, setAppReady] = useState(false);
+  const { setUser, clearUser } = useUserStore();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), async user => {
+      if (user) {
+        try {
+          const data = await getUser(user);
+          setUser(data as TEmpUserData | TCMUserData);
+        } catch (e) {
+          clearUser();
+        }
+      } else {
+        clearUser();
+      }
+
+      setAppReady(true);
+    });
+
+    return () => unsubscribe();
+  }, [setUser, clearUser]);
+
+  if (!appReady) return <Loading />;
+
+  return <RouterProvider router={MainRoutes} />;
 };
 
 export default App;
