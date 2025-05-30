ea284a4f83665c4854d8f365b0e90cb8e96b9f8c
mintaek <omt610@gmail.com>
2025-05-31 01:27:58 +0900

    🐛Fix: 직원 휴가 페이지네이션 위치 수정

--- src/Components/employee/vacation/EmployeeVacationListSection.tsx
+++ src/Components/employee/vacation/EmployeeVacationListSection.tsx
@@ -28,16 +28,18 @@ const VacationListSection = ({
   return (
     <>
       <EmployeeVacationList key={currentPage} requests={requests} paginated />
-      <Pagination
-        page={currentPage}
-        totalPageCount={totalPageCount}
-        onNext={() => {
-          setCurrentPage(Math.min(currentPage + 1, totalPageCount - 1));
-        }}
-        onPrevious={() => {
-          setCurrentPage(Math.max(currentPage - 1, 0));
-        }}
-      />
+      <div className="-translate-y-8">
+        <Pagination
+          page={currentPage}
+          totalPageCount={totalPageCount}
+          onNext={() => {
+            setCurrentPage(Math.min(currentPage + 1, totalPageCount - 1));
+          }}
+          onPrevious={() => {
+            setCurrentPage(Math.max(currentPage - 1, 0));
+          }}
+        />
+      </div>
     </>
   );
 };
