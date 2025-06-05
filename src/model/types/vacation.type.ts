e1209bba143100c618f554ad84645d42db9a0141
mintaek <omt610@gmail.com>
2025-06-05 19:31:09 +0900

    🚑!HOTFIX: 휴가 status 통일

--- src/model/types/vacation.type.ts
+++ src/model/types/vacation.type.ts
@@ -2,7 +2,7 @@ import { TJobList, TSelectableJobName } from "./company.type";
 import { TUserBase } from "./user.type";
 
 export type TVacationType = "반차" | "연차" | "특별";
-export type TVacationStatus = "대기중" | "승인" | "거절" | "자동 승인";
+export type TVacationStatus = "대기중" | "승인" | "거절" | "자동승인";
 
 // 휴가 요청 타입
 export type TVacationRequest<T extends TJobList = TJobList> = {
@@ -12,7 +12,7 @@ export type TVacationRequest<T extends TJobList = TJobList> = {
   startDate: string; // "YYYY-MM-DD"
   endDate: string; // "YYYY-MM-DD"
   reason: string;
-  status: Exclude<TVacationStatus, "자동 승인됨">; // "대기중" | "승인됨" | "거절됨"
+  status: Exclude<TVacationStatus, "자동승인">; // "대기중" | "승인됨" | "거절됨"
   createdAt: string; // ISO 문자열 (저장은 문자열로, 필요시 date로 객체 변환)
   processedAt?: string; // 처리된 시간 (승인/거절 시점)
 };
@@ -23,7 +23,7 @@ export type TRegisteredVacation<T extends TJobList = TJobList> = {
   startDate: string;
   endDate: string;
   vacationType: TVacationType;
-  status: "자동 승인됨" | "승인";
+  status: "자동승인" | "승인";
   reason: string;
   createdAt: string;
 } & Pick<TUserBase, "name" | "email"> & {
